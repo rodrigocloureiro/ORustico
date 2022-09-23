7d03fa5398ff01ca03acde6a5c46d826d6b0cb4f
rodrigocloureiro <110643385+rodrigocloureiro@users.noreply.github.com>
2022-09-23 20:13:54 -0300

    final version
    projeto o rustico - com responsividade

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// alert("Hello World!");
+
+var btnMenu = document.querySelector(".btn-menu");
+var menu = document.querySelector(".menu");
+
+// console.log(btnMenu);
+
+btnMenu.addEventListener("click", function() {
+    // alert("Você clicou no menu!");
+    // menu.classList.add("menu-open");
+    // menu.classList.remove("menu-open");
+    menu.classList.toggle("menu-open");
+});
